dd2c7afc4e4560bcbd984e2c875fcc6035b49a3e
Ben Mohammad <benmohammad593@gmail.com>
2020-06-22 19:09:02 +0100

    TodoViewModel added...

---
+++ app/src/main/java/com/benmohammad/mvvmapplication/viewmodel/TodoViewModel.kt
@@ -0,0 +1,38 @@
+package com.benmohammad.mvvmapplication.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import com.benmohammad.mvvmapplication.data.TodoItemRepository
+import com.benmohammad.mvvmapplication.data.database.TodoItem
+
+class TodoViewModel(application: Application): AndroidViewModel(application) {
+
+    private val repository: TodoItemRepository = TodoItemRepository(application)
+    private val todoItems: LiveData<MutableList<TodoItem>> = repository.getAllTodoList()
+
+    fun saveTodoItem(todoItem: TodoItem) {
+        repository.saveTodoItem(todoItem)
+    }
+
+    fun saveTodItems(todoItems: List<TodoItem>) {
+        repository.saveTodoItems(todoItems)
+    }
+
+    fun updateTodoItem(todoItem: TodoItem) {
+        repository.updateTodoItem(todoItem)
+    }
+
+    fun deleteTodoItem(todoItem: TodoItem) {
+        repository.deleteTodoItem(todoItem)
+    }
+
+    fun toggleCompleteState(todoItem: TodoItem) {
+        todoItem.completed = !todoItem.completed
+        repository.updateTodoItem(todoItem)
+    }
+
+    fun getAllTodoItemList(): LiveData<MutableList<TodoItem>> {
+        return todoItems
+    }
+}
